7af5189b9930d18ac6c1fcfcecb398a1116f2615
Shhhwepsss <xomai.ars@gmail.com>
2022-05-05 10:40:46 +0300

    second-commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 import { store } from "./store/store"
 import { Provider } from 'react-redux';
@@ -15,14 +13,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
